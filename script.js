5999fc87718cace72266778c54392bc65355c0b7
nguyen89mke <nguyen89.mke@gmail.com>
2022-07-12 21:54:32 -0700

    Updated JavaScript Code

--- script.js
+++ script.js
@@ -16,11 +16,18 @@ class Calculator {
     }
 
     appendNumber(number) {
-
+        if (number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
     }
 
     chooseOperation(operation) {
-
+        if (this.currentOperand === '') return
+        if (this.previousOperand !== '') {
+            this.compute()
+        }
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
     }
 
     compute() {
@@ -28,6 +35,8 @@ class Calculator {
     }
 
     updateDisplay() {
+        this.currentOperandTextElement.innerText = this.currentOperand
+        this.previousOperandTextElement.innerText = this.previousOperand
 
     }
 }
@@ -35,14 +44,22 @@ class Calculator {
 const numberButtons = document.querySelectorAll('[data-number')
 const operationButtons = document.querySelectorAll('[data-operation')
 const equalsButton = document.querySelector('[data-equals')
-const deleteButton = document.querySelector('[data-equals')
+const deleteButton = document.querySelector('[data-delete')
 const allClearButton = document.querySelector('[data-all-clear')
 const previousOperandTextElement = document.querySelector('[data-previous-operand')
-const xurrentOperandTextElement = document.querySelector('[data-current-operand')
+const currentOperandTextElement = document.querySelector('[data-current-operand')
 
-const calculator = new Calculator(previousOperandTextElement, currentOperand) 
+const calculator = new Calculator(previousOperandTextElement, 
+    currentOperandTextElement) 
 
 numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+operationButtons.forEach(button => {
     button.addEventListener('click', () => {
         calculator.appendNumber(button.innerText)
         calculator.updateDisplay()
